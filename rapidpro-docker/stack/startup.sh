19458f1c70832ffb0c3ab9a3c246771e1554dded
Daniel Castelao Suárez <48926694+dcastelaosua@users.noreply.github.com>
2022-10-02 22:00:59 +0200

    Update startup.sh

--- rapidpro-docker/stack/startup.sh
+++ rapidpro-docker/stack/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex # fail on any error & print commands as they're run
 
-cp /rapidpro/env/static/*.* /rapidpro/env/sitestatic/*.*
+#cp /rapidpro/env/static/*.* /rapidpro/env/sitestatic/*.*
 
 if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
 	/rapidpro/env/bin/python manage.py collectstatic --noinput --no-post-process
