8634c9da88dfb4aa8bf28882500b87e25cd8025b
Daniel Castelao Suárez <48926694+dcastelaosua@users.noreply.github.com>
2022-10-02 22:52:55 +0200

    Update startup.sh

--- rapidpro-docker/stack/startup.sh
+++ rapidpro-docker/stack/startup.sh
@@ -2,17 +2,18 @@
 set -ex # fail on any error & print commands as they're run
 
 #cp /rapidpro/env/static/*.* /rapidpro/env/sitestatic/*.*
+/bin/bash -c 'source /rapidpro/env/bin/activate'
 
 if [ "x$MANAGEPY_COLLECTSTATIC" = "xon" ]; then
-	/rapidpro/env/bin/python manage.py collectstatic --noinput --no-post-process
+	python manage.py collectstatic --noinput --no-post-process
 fi
 if [ "x$CLEAR_COMPRESSOR_CACHE" = "xon" ]; then
-	/rapidpro/env/bin/python clear-compressor-cache.py
+	python clear-compressor-cache.py
 fi
-#if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
-#	/rapidpro/env/bin/python manage.py compress --extension=".haml" --force -v0
-#fi
-/bin/bash -c 'source /rapidpro/env/bin/activate'
+if [ "x$MANAGEPY_COMPRESS" = "xon" ]; then
+	python manage.py compress --extension=".haml" --force -v0
+fi
+#/bin/bash -c 'source /rapidpro/env/bin/activate'
 if [ "x$MANAGEPY_INIT_DB" = "xon" ]; then
 	#/rapidpro/env/bin/python manage.py dbshell < init_db.sql
 	psql -U postgres -h postgresql -tc "SELECT 1 FROM pg_user WHERE usename = 'temba'" | grep -q 1 || psql -U postgres -h postgresql -c "CREATE USER temba WITH SUPERUSER encrypted password 'temba'"
@@ -28,8 +29,8 @@ if [ "x$MANAGEPY_MIGRATE" = "xon" ]; then
 fi
 if [ "x$MANAGEPY_IMPORT_GEOJSON" = "xon" ]; then
 	echo "Downloading geojson for relation_ids $OSM_RELATION_IDS"
-	/rapidpro/env/bin/python manage.py download_geojson $OSM_RELATION_IDS
-	/rapidpro/env/bin/python manage.py import_geojson ./geojson/*.json
+	python manage.py download_geojson $OSM_RELATION_IDS
+	python manage.py import_geojson ./geojson/*.json
 	echo "Imported geojson for relation_ids $OSM_RELATION_IDS"
 fi
 $STARTUP_CMD 
