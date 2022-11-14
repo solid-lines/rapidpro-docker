cfce76eb0f786d92b7908dc00bd63ce58809e8e9
Daniel Castelao Suárez <48926694+dcastelaosua@users.noreply.github.com>
2022-11-14 23:02:04 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,11 @@
 
 HOSTNAME=$(grep HOSTNAME .env | awk -F '=' '{printf $2}')
 RP_CONTAINERS=$(docker container ls | grep ${HOSTNAME} | awk '{printf $2"\n" }' | sort | uniq | awk -F ':' '{print $1":"$2" "}')
+if [[ $RP_CONTAINERS == "" ]]; then
+  echo "There are no Rapidpro containers with hostname: ${HOSTNAME}" 
+  exit 1
+fi
+
 echo "Stopping Rapidpro docker containers..."
 if ! docker-compose down; then
   echo "Failed docker-compose down"
